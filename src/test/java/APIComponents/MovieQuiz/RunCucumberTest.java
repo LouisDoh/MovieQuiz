3dbd54f0b8de48210ef13f144a047dcf4c4f5b65
Louis Doherty <louisdoherty98@gmail.com>
2022-11-01 14:54:03 +0000

    Created file for cucumber test (currently unused)

---
+++ src/test/java/APIComponents/MovieQuiz/RunCucumberTest.java
@@ -0,0 +1,11 @@
+//package APIComponents.MovieQuiz;
+//import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.plugin.Plugin;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(plugin = {"pretty"},
+//features = "src/test/resources/Cucmber",
+//glue = )
+//public class runCucumberTest {
+//}
--- src/test/java/APIComponents/MovieQuiz/runCucumberTest.java
+++
@@ -1,2 +0,0 @@
-package APIComponents.MovieQuiz;public class runCucumberTest {
-}
